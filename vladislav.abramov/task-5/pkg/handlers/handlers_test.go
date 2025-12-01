61dcb6dc280bc1195806b2b714f5b4f8115a000e
15446-rus75 <vladabramov06@yandex.ru>
2025-12-01 21:46:56 +0300

    [TASK-6] add import

--- vladislav.abramov/task-5/pkg/handlers/handlers_test.go
+++ vladislav.abramov/task-5/pkg/handlers/handlers_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 	"time"
 
+  "github.com/15446-rus75/task-5/pkg/conveyer"
+  "github.com/15446-rus75/task-5/pkg/handler"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
